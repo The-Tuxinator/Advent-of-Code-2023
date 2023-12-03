95d1620b1a68643f3993cf3986f18aad2e4735a2
andy <andy.f.james@protonmail.com>
2023-12-03 01:20:25 -0500

    Day 3: List casting worked

--- day3/day3_1.py
+++ day3/day3_1.py
@@ -3,27 +3,17 @@ from pprint import pp
 
 
 def parse_numbers(line):
-    search_result = re.search(r"\d+", line)
-    if search_result is None:
-        return
-    number_list
-    last_match = 0
-    while True:
-        search_result = re.search(r"\d+", line[number_list[-1].start()+1])
-        if
-        number_list.append()
-    print number_list
-    return re.finditer(r"\d+", line)
+    return list(re.finditer(r"\d+", line))
 
 
 def parse_symbols(line):
-    return re.finditer(r"[^\d|.|\n]", line)
+    return list(re.finditer(r"[^\d|.|\n]", line))
 
 
 def has_adjacent_symbol(number, symbols):
     for symbol in symbols:
         if number.start()-1 <= symbol.start() <= number.end()+1:
-            return int(number.group(1))
+            return int(number.group(0))
     return 0
 
 
@@ -33,14 +23,15 @@ with open("input3", "r") as input_file:
     for each in input_file.readlines():
         rows.append([parse_numbers(each), parse_symbols(each)])
 
-pp(rows)
+# pp(rows)
 
 for i, row in enumerate(rows):
     symbols = row[1]
     if i > 0:
         symbols = symbols + rows[i-1][1]
-    if i < len(rows):
+    if i < len(rows)-1:
         symbols = symbols + rows[i+1][1]
     for number in row[0]:
         part_numbers.append(has_adjacent_symbol(number, symbols))
 print(part_numbers)
+print(f"The sum is {sum(part_numbers)}")
